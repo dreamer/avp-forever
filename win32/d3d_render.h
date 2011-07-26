107b55be48868ffdeebfe3dce8d2ca2006b2933e
Barry Duncan <sirlemonhead@outlook.com>
2011-07-26 00:19:09 +0000

    - added kshape.h include to d3d_render.h and some function prototypes
    Imported-from: https://subversion.assembla.com/svn/AvP/branches/workbranch/trunk@453

--- win32/d3d_render.h
+++ win32/d3d_render.h
@@ -1,6 +1,8 @@
 #ifndef _included_d3d_render_h_ /* Is this your first time? */
 #define _included_d3d_render_h_ 1
 
+#include "kshape.h"
+
 extern void D3D_DrawBackdrop(void);
 extern void PostLandscapeRendering(void);
 extern void D3D_DrawWaterTest(MODULE *testModulePtr);
@@ -16,11 +18,21 @@ extern void D3D_Decal_Output(DECAL *decalPtr,RENDERVERTEX *renderVerticesPtr);
 extern void D3D_Particle_Output(PARTICLE *particlePtr,RENDERVERTEX *renderVerticesPtr);
 extern void D3D_DrawParticle_Rain(PARTICLE *particlePtr,VECTORCH *prevPositionPtr);
 
+void D3D_Rectangle(int x0, int y0, int x1, int y1, int r, int g, int b, int a);
+extern void DrawMenuTextGlow(uint32_t topLeftX, uint32_t topLeftY, uint32_t size, uint32_t alpha);
+extern void D3D_RenderHUDString(char *stringPtr, int x, int y, int colour);
+
 extern void D3D_DecalSystem_Setup(void);
 extern void D3D_DecalSystem_End(void);
 
 extern void D3D_FadeDownScreen(int brightness, int colour);
 extern void D3D_PlayerOnFireOverlay(void);
+
+void ThisFramesRenderingHasBegun();
+void ThisFramesRenderingHasFinished();
+extern void D3D_DrawSliderBar(int x, int y, int alpha);
+extern void D3D_DrawSlider(int x, int y, int alpha);
+extern void D3D_FadeDownScreen(int brightness, int colour);
 
 void D3D_Rectangle(int x0, int y0, int x1, int y1, int r, int g, int b, int a);
 
